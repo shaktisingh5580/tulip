7f7841885fa72fe933e2a29845619eb0331b50a5
shaktisingh5580 <shaktisingh7227077@gmail.com>
2024-07-16 12:37:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+const tulipField = document.querySelector('.tulip-field');
 
+function createTulip() {
+    const tulip = document.createElement('div');
+    tulip.classList.add('tulip');
+    
+    const stem = document.createElement('div');
+    stem.classList.add('stem');
+    
+    const flower = document.createElement('div');
+    flower.classList.add('flower');
+    
+    tulip.appendChild(stem);
+    tulip.appendChild(flower);
+    
+    tulipField.appendChild(tulip);
+}
+
+// Create multiple tulips
+for (let i = 0; i < 200; i++) {
+    createTulip();
+}
